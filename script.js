cc266312af42ba053e834b75fc2ba2e65743b240
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-16 22:09:12 +0600

    fetched api

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const cityInput = document.querySelector('.city-input');
 const searchBtn = document.querySelector('.search-btn');
+const apiKey = 'b47a9784cfb6db31c0e370e9e524e9fd'
 
 searchBtn.addEventListener('click', () => {
-    console.log(cityInput.value);
+    if(cityInput.value.trim() != ''){
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = ''
+        cityInput.blur()
+    }
+    
 })
+cityInput.addEventListener('keydown', (event) => {
+    if (event.key == 'Enter' && 
+        cityInput.value.trim() != ''
+    ) {
+        updateWeatherInfo(cityInput.value)
+        cityInput.value = ''
+        cityInput.blur()
+    }
+})
+
+async function getFetchData(endPoint, city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}`
+    const response = await fetch(apiUrl)
+    return response.json()
+}
+
+async function updateWeatherInfo(city) {
+    const weatherData = await getFetchData('weather', city)
+    console.log(weatherData);
+}
