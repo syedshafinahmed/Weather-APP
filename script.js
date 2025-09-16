0a6bb412a27834ce513db536e0bc0f7782520040
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-16 19:22:11 +0600

    added js file

---
+++ script.js
@@ -0,0 +1,6 @@
+const cityInput = document.querySelector('.city-input');
+const searchBtn = document.querySelector('.search-btn');
+
+searchBtn.addEventListener('click', () => {
+    console.log(cityInput.value);
+})
